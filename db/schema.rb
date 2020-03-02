90ee30dd09dcd85c0a5d332b962d1c9bfd66f68a
morgan hoare <hoaremorgan@gmail.com>
2020-03-02 14:26:59 -0300

    added user to trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_152323) do
+ActiveRecord::Schema.define(version: 2020_03_02_171629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2020_03_02_152323) do
     t.string "purpose"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,4 +64,5 @@ ActiveRecord::Schema.define(version: 2020_03_02_152323) do
 
   add_foreign_key "subtasks", "tasks"
   add_foreign_key "tasks", "trips"
+  add_foreign_key "trips", "users"
 end
