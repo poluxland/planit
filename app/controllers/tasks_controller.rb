f7baf5cdf9b1c5d0df08b599ef14a342c548041b
Manuel Scholz <manuscho@live.de>
2020-03-03 16:34:49 -0300

    minor edits

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,49 +1,48 @@
 class TasksController < ApplicationController
-    def new           # GET /restaurants/new
-      @task = Task.new
-      @trip = Trip.find(params[:trip_id])
-      authorize @task
+  def new           # GET /restaurants/new
+    @task = Task.new
+    @trip = Trip.find(params[:trip_id])
+    authorize @task
+  end
+
+  def create        # POST /restaurants
+    @task = Task.new(task_params)
+    @trip = Trip.find(params[:trip_id])
+    @task.trip = @trip
+    @task.status = "open"
+    authorize @task
+    if @task.save
+      redirect_to trip_path(@trip)
+    else
+      render :new
     end
+  end
 
-    def create        # POST /restaurants
-      @task = Task.new(task_params)
-      @trip = Trip.find(params[:trip_id])
-      @task.trip = @trip
-      @task.status = "open"
-      authorize @task
-      if @task.save
-        redirect_to trip_path(@trip)
-      else
-        render :new
-      end
+  def edit          # GET /restaurants/:id/edit
+    @task = Task.find(params[:id])
+    authorize @task
+  end
+
+  def update        # PATCH /restaurants/:id
+    @task = Task.find(params[:id])
+    authorize @task
+    if @task.update(task_params)
+      redirect_to trip_path(@task.trip)
+    else
+      render :edit
     end
+  end
 
-    def edit          # GET /restaurants/:id/edit
-      @task = Task.find(params[:id])
-      authorize @task
-    end
+  def destroy
+   @task = Task.find(params[:id])
+   authorize @task
+   @task.destroy
+   redirect_to trip_path(@task.trip)
+  end
 
-    def update        # PATCH /restaurants/:id
-      @task = Task.find(params[:id])
-      authorize @task
-      if @task.update(task_params)
-        redirect_to trip_path(@task.trip)
-      else
-        render :edit
-      end
+  private
 
-    end
-
-    def destroy       # DELETE /restaurants/:id
-     @task = Task.find(params[:id])
-     authorize @task
-     @task.destroy
-     redirect_to trip_path(@task.trip)
-   end
-
-   private
-
-   def task_params
+  def task_params
     params.require(:task).permit(:name, :description, :task_id)
   end
 
