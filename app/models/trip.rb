a7282d904afdef4bba454f8dd93cb1ae6b1ab823
benjamin debonneville <b.debonneville@gmail.com>
2020-03-03 10:54:22 -0300

    add validation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,7 @@ class Trip < ApplicationRecord
   has_many :tasks, dependent: :delete_all
   belongs_to :user, optional: true
 
+  # Validation
+  validates :name,  presence: :true
+  validates :description,  presence: :true
 end
