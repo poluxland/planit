55ae1d1dcab25907db105781a5e5d5b7e95822ab
Manuel Scholz <manuscho@live.de>
2020-03-03 12:07:51 -0300

    New dashbaord

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,6 @@ class Trip < ApplicationRecord
   has_many :tasks, dependent: :delete_all
   belongs_to :user, optional: true
 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
