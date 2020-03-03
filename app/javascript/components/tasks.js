d49bb4edbc6ae44e1cfaf58bba004b92eb3744b5
Jose Jerez <joseignaciojerez@gmail.com>
2020-03-03 14:45:52 -0300

    fix tasks.js

--- app/javascript/components/tasks.js
+++ app/javascript/components/tasks.js
@@ -1,14 +1,14 @@
-// const initOpenTaskOnClick = () => {
-  const tasks = document.querySelectorAll('.task-container');
-  // const subtasks = document.querySelectorAll('.subtask');
+// // const initOpenTaskOnClick = () => {
+//   const tasks = document.querySelectorAll('.task-container');
+//   // const subtasks = document.querySelectorAll('.subtask');
 
-  // if (tasks) {
-    tasks.forEach((task) => {
-      task.addEventListener('click', () => {
-        alert(task)
-      });
-    });
-  // }
-// }
+//   // if (tasks) {
+//     tasks.forEach((task) => {
+//       task.addEventListener('click', () => {
+//         alert(task)
+//       });
+//     });
+//   // }
+// // }
 
-export { initOpenTaskOnClick };
+// export { initOpenTaskOnClick };
