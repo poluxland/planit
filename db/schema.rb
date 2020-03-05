ba31e1c80bc03fbd70f8f8579a0ac0e720b3a31e
Manuel Scholz <manuscho@live.de>
2020-03-05 12:17:36 -0300

    Bugfixes and delete trip icon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_210631) do
+ActiveRecord::Schema.define(version: 2020_03_05_145458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2020_03_04_210631) do
     t.float "latitude"
     t.float "longitude"
     t.text "description"
+    t.boolean "session", default: false
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
