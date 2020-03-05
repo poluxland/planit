00dcca73f122af8895ec38b16cf5aac029247c6a
Jose Jerez <joseignaciojerez@gmail.com>
2020-03-05 12:34:27 -0300

    add app

---
+++ app/app/create_trips/accomodation.rb
@@ -0,0 +1,15 @@
+def accomodation(trip)
+  # Create Task
+  @task = Task.new(tip: nil, name: "Book your accomodation", description: "Make sure you have a place to stay for at least the first few days - Immigration might ask you for it.")
+  @task.trip = trip
+  @task.save
+
+  def save_subtask
+    subtask = Subtask.new(name: @name,description: @description)
+    subtask.task = @task
+    subtask.save
+  end
+
+  # Create Subtask S
+
+end
