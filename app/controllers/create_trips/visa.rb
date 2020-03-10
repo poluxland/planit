7243264447139d5a96698ce19d07a7eada84a21a
Jose Jerez <joseignaciojerez@gmail.com>
2020-03-10 11:37:25 -0300

    visa working with exceptions

--- app/controllers/create_trips/visa.rb
+++ app/controllers/create_trips/visa.rb
@@ -2,6 +2,12 @@ def visa(trip)
   # Create Task
   require 'csv'
 
+  def save_subtask
+    subtask = Subtask.new(name: @name,description: @description)
+    subtask.task = @task
+    subtask.save
+  end
+
 
 
   @arrival_destination = @trip.location.split(', ')[-1].downcase.capitalize!
@@ -10,26 +16,44 @@ def visa(trip)
   table = CSV.parse(File.read("#{Rails.root}/public/passport.csv"), headers: true)
   row = table.find {|row| row['Passport'] == "#{@departure_destination}" && row["Destination"] == "#{@arrival_destination}"}
 
-
-  code = row['Code']
-
-
-  @task = Task.new(tip: nil, name: "Visa requirements", description: "If you are from #{@departure_destination} and you travel to #{@arrival_destination} you can travel for #{code} days without a Visa.")
-  @task.trip = trip
-  @task.save
-
-
-
-
-  def save_subtask
-    subtask = Subtask.new(name: @name,description: @description)
-    subtask.task = @task
-    subtask.save
-  end
-
+  if row.nil?
+    @task = Task.new(tip: nil, name: "Visa requirements", description: "You need to check the requirements with the country")
+    @task.trip = trip
+    @task.save
 
     @name = "Get your passport!"
     @description = "Check the expiration date"
     save_subtask
+
+    @name = "Get your Visa!"
+    @description = "Check the requirements with the embassy"
+    save_subtask
+
+  else
+    code = row['Code']
+
+    if code.to_s.scan(/\D/).empty?
+      @task = Task.new(tip: nil, name: "Visa requirements", description: "If you are from #{@departure_destination} and you travel to #{@arrival_destination} you can travel for #{code} days without a Visa.")
+      @task.trip = trip
+      @task.save
+
+      @name = "Get your passport!"
+      @description = "Check the expiration date"
+      save_subtask
+    else
+      @task = Task.new(tip: nil, name: "Visa requirements", description: "If you are from #{@departure_destination} and you travel to #{@arrival_destination} you need a Visa")
+      @task.trip = trip
+      @task.save
+
+      @name = "Get your passport!"
+      @description = "Check the expiration date"
+      save_subtask
+
+      @name = "Get your Visa!"
+      @description = "Check the requirements with the embassy"
+      save_subtask
+
+      end
+  end
 
 end
