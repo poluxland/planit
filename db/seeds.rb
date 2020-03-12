8d2fc79ae1a3e190d7c59bd45bd81e6a37f2bd69
morgan hoare <hoaremorgan@gmail.com>
2020-03-12 13:25:50 -0300

    eliminated seeds useless

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,32 +14,4 @@ jose = User.create!(email: "jose@planit.wtf", password: "testtest", first_name: 
 ben = User.create!(email: "ben@planit.wtf", password: "testtest", first_name: "Ben")
 manuel = User.create!(email: "manuel@planit.wtf", password: "testtest", first_name: "Manuel")
 
-Trip.create!({start_date: Time.now.to_date,
-             end_date: (Time.now.to_date + 20),
-             gender: 'female',
-             age: 29,
-             origin: 'Buenos Aires',
-             purpose: 'pleasure',
-             location: 'Paris, France',
-             name: 'my trip to paris',
-             description: 'This is my trip to paris, I am going next year',
-             user_id: User.first.id})
-
-Task.create!({
-  trip_id: Trip.first.id,
-  name: "Visa",
-  description: "Go get that Visa!",
-  status: "open",
-  tip: "tip"
-})
-
-Task.create!({
-  trip_id: Trip.first.id,
-  name: "Prepare bag",
-  description: "What are you waiting for? The last minute?",
-  status: "open",
-  tip: "tip"
-})
-
-chatroom = ChatRoom.create(name: 'chatroom')
 
